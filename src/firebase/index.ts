ba06fbbf68e844de8a603b2af1c97d9220aa3945
Adrija Dey <adrijadeyslg@gmail.com>
2025-10-17 08:19:59 +0000

    Set up a Firebase backend

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';

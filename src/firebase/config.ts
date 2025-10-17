ee1915c44579b1f6cf642c8c4fb7d0d54b986605
Adrija Dey <adrijadeyslg@gmail.com>
2025-10-17 08:00:42 +0000

    this error is coming FIX ITTTTTTT

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6325736797-8e001",
-  "appId": "1:619744485051:web:f2e2b52efa51421daf539e",
-  "apiKey": "AIzaSyDKSxodgozBMSjgwBKPrMn35rEHVehyZ3c",
-  "authDomain": "studio-6325736797-8e001.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "619744485051"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
